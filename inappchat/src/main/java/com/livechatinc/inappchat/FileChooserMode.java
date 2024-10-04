0a001d9009a972fad5eda6e643a0c6b1d82de78f
SzymonJarosz <nomyzs@gmail.com>
2024-10-04 13:56:23 +0200

    remove remaining SDK condition, handle some lint warnings

--- inappchat/src/main/java/com/livechatinc/inappchat/FileChooserMode.java
+++ inappchat/src/main/java/com/livechatinc/inappchat/FileChooserMode.java
@@ -1,11 +1,6 @@
 package com.livechatinc.inappchat;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
-enum FileChooserMode {
+public enum FileChooserMode {
     SINGLE,
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     MULTIPLE
 }
