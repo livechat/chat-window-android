2ba34dc6daae7e01b052b8d1017fc61ff0fa8596
SzymonJarosz <nomyzs@gmail.com>
2025-03-28 12:45:01 +0100

    indicate callbacks executed on the main thread

--- ChatWidget/src/main/java/com/livechatinc/chatwidget/src/LiveChatViewCallbackListener.kt
+++ ChatWidget/src/main/java/com/livechatinc/chatwidget/src/LiveChatViewCallbackListener.kt
@@ -1,11 +1,21 @@
 package com.livechatinc.chatwidget.src
 
+import androidx.annotation.MainThread
 import com.livechatinc.chatwidget.src.models.ChatMessage
 
 interface LiveChatViewCallbackListener {
+    @MainThread
     fun onLoaded()
+
+    @MainThread
     fun onHide()
+
+    @MainThread
     fun onNewMessage(message: ChatMessage?)
+
+    @MainThread
     fun onError(cause: Throwable)
+
+    @MainThread
     fun onFileChooserActivityNotFound()
 }
