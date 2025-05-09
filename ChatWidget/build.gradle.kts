005ec36acee62643e5f785f781b99caefc20c46d
SzymonJarosz <nomyzs@gmail.com>
2025-05-09 15:58:13 +0200

    remove not needed constraintlayout dependency

--- ChatWidget/build.gradle.kts
+++ ChatWidget/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity:1.9.3")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
