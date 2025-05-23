62cee5d7dcf9acaa9e1334f12630fa027d23b85c
SzymonJarosz <nomyzs@gmail.com>
2025-05-23 16:00:55 +0200

    remove not needed serialization names

--- ChatWidget/src/main/java/com/livechatinc/chatwidget/src/models/ChatWidgetToken.kt
+++ ChatWidget/src/main/java/com/livechatinc/chatwidget/src/models/ChatWidgetToken.kt
@@ -1,16 +1,14 @@
 package com.livechatinc.chatwidget.src.models
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-
 
 @Serializable
 internal data class ChatWidgetToken(
-    @SerialName("accessToken") var accessToken: String? = null,
-    @SerialName("entityId") var entityId: String? = null,
-    @SerialName("expiresIn") var expiresIn: Int? = null,
-    @SerialName("licenseId") var licenseId: String? = null,
-    @SerialName("tokenType") var tokenType: String? = null,
-    @SerialName("creationDate") var creationDate: Long? = null,
+    var accessToken: String? = null,
+    var entityId: String? = null,
+    var expiresIn: Int? = null,
+    var licenseId: String? = null,
+    var tokenType: String? = null,
+    var creationDate: Long? = null,
 )
 
